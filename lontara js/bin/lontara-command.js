1e229a5f765b77adbaae4fa271722b5ba9229db7
Asrul Ramadani <68553467+Asrulramadani@users.noreply.github.com>
2025-03-23 23:17:32 +0800

    update project lontara

--- lontara js/bin/lontara-command.js
+++ lontara js/bin/lontara-command.js
@@ -6,17 +6,16 @@ const path = require("path");
 // Ambil file dari argumen CLI
 const filePath = process.argv[2];
 if (!filePath) {
-  console.error("Harap masukkan file Lontara seperti: ᨍᨆᨕᨗ  index.ᨒᨚᨈᨑ");
+  console.error("Harap masukkan file Lontara seperti: ᨍᨆᨕᨗ index.ᨒᨚᨈᨑ");
   process.exit(1);
 }
 
 // Baca file
-const ogiCode = fs.readFileSync(filePath, "utf-8");
+const lontaraCode = fs.readFileSync(filePath, "utf-8");
 
 // Import interpreter
 const { translateAndRun } = require("../interpreter");
 
-
-
 // Jalankan kode
-translateAndRun(ogiCode);
+translateAndRun(lontaraCode);
+
